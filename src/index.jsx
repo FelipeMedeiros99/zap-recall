8182b25e7cb4d23adf17a0cc33afe75762f5ae1e
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2024-04-02 21:19:32 -0300

    iniciando projeto, configurações iniciais

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import { ReactDOM } from 'react-dom'
+import './estilos/reset.css'
+import './estilos/index.css'
+
+const App = ()=>{
+    return(<></>)
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.root'))
+
